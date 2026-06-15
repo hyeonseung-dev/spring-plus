a8ea7aef81d0af0a3af78afc25f3cc087c996f7f
HS <ehehqp@gmail.com>
2026-06-15 14:49:11 +0900

    과제 레벨3-11. Transaction 심화
    - H2 시큐리티 설정허용

--- src/main/java/org/example/expert/config/SecurityConfig.java
+++ src/main/java/org/example/expert/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package org.example.expert.config;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,10 +22,14 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
 			.csrf(csrf -> csrf.disable())
+			.headers(headers -> headers
+				.frameOptions(frameOptions -> frameOptions.disable())
+			)
 			.sessionManagement(session ->
 				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			)
 			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(PathRequest.toH2Console()).permitAll()
 				.requestMatchers("/auth/signup", "/auth/signin").permitAll()
 				.requestMatchers("/admin/**").hasRole("ADMIN")
 				.anyRequest().authenticated()
